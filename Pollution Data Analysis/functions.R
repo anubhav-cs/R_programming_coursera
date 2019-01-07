969e45b1c755af0e0fba8b69394bab449089a118
anubhav <anubhavs@student.unimelb.edu.au>
2019-01-07 16:32:06 +1100

    Finished code file

--- Pollution Data Analysis/functions.R
+++ Pollution Data Analysis/functions.R
@@ -31,11 +31,10 @@ pollutantmean   <-  function(directory, pollutant, id = 1:332){
     #       ignoring any missing values coded as NA
     #####
     filenames   <-  list.files(directory, pattern="*.csv", full.names=TRUE)
-    ldf <- lapply(filenames, read.csv) #dataframes for each file
-
-    sum <- 0
-    count <- 0
-    for(i in id){
+    ldf         <-  lapply(filenames, read.csv) #dataframes for each file
+    sum         <-  0
+    count       <-  0
+    for (i in id) {
         # remove NA values in pollutant column
         temp    <-  ldf[[i]][pollutant][!is.na(ldf[[i]][pollutant])]
         count   <-  count + length(temp)
@@ -53,20 +52,43 @@ complete    <-  function(directory, id = 1:332){
     #                    , for each file
 
     filenames   <-  list.files(directory, pattern="*.csv", full.names=TRUE)
-    ldf         <- lapply(filenames, read.csv) #dataframes for each file
+    ldf         <-  lapply(filenames, read.csv) #dataframes for each file
+    idvec       <-  NULL
+    nobsvec     <-  NULL
 
-    idvec      <-  NULL
-    nobsvec    <-  NULL
-    for(i in id){
+    for (i in id) {
         # remove records where any record has NA values
         temp    <-  na.omit(ldf[[i]])
         count   <-  nrow(temp)
-        idvec  <-  c(idvec, i)
-        nobsvec<-  c(nobsvec, count)
+        idvec   <-  c(idvec, i)
+        nobsvec <-  c(nobsvec, count)
     }
     data.frame(id = idvec, nobs = nobsvec) #create dataframe
 }
 
-corr <- function(directory, threshold = 0){
+corr    <-  function(directory, threshold = 0){
+    # Args:
+    #       directory(charater vector) - location of csv files
+    #       threshold(integer value)   - minimum count of complete observations
+    #                                   from a monitor/ sensor in a file
+    # Returns:
+    #       (numeric vector) - correlations
 
+    filenames   <-  list.files(directory, pattern="*.csv", full.names=TRUE)
+    ldf         <-  lapply(filenames, read.csv) #dataframes for each file
+    sulfatevec  <-  vector("numeric")
+    nitratevec  <-  vector("numeric")
+    corrvec     <-  NULL
+
+    for (i in 1:length(ldf)) {
+        # remove records where any record has NA values
+        temp        <-  na.omit(ldf[[i]])
+        count       <-  nrow(temp)
+
+        if (count <= threshold) next
+        else
+          corrvec   <-  c(corrvec, cor(temp[["sulfate"]], temp[["nitrate"]]))
+    }
+
+    corrvec #correlations vector
 }
